31f53727eccecf1a289563a66edd5b3e36b36cda
Will <Will.r.smart@gmail.com>
2012-08-20 21:15:49 +1200

    some revamp of LongPlay. Fixed a bug somehow and there are now 62 winners

--- LongPlay.java
+++ LongPlay.java
@@ -1,42 +1,71 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class LongPlay {
+	public static boolean doTests=false;
+
+	public static long patternFromArray(Boolean[][] map) {
+		long ret=0;
+		long bit=1;
+		for (int y=0;y<8;y++) {
+			for (int x=0;x<8;x++,bit<<=(ret==0?0:1)) {
+				if ((map.length>y)&&(map[y].length>x)&&map[y][x]) {
+					ret|=bit;
+				}
+			}
+		}
+		return(ret);
+	}
+
+	public static boolean canShiftWithoutWrappingOrClipping(long block,int shift) {
+		int topBiti=0;for (int bi=0;bi<64;bi++) if ((block&(1L<<bi))!=0) topBiti=bi;
+		return(
+				(shift+topBiti<64)&& // i.e. no clipping of end
+				(!((((block<<shift)&0x0101010101010101L)!=0)&&  // i.e. not touching both side wals at the same time
+					(((block<<shift)&0x8080808080808080L)!=0)))
+			);
+	}
+	
+	
+	
+	Block[] blocks=stGrabBlocks();
+	
+	public static Block[] stGrabBlocks() {
+		List<Piece> pieces=PieceFactory.puzzlePieces1();
+		int btIndex=0;
+		List<Block> blks=new ArrayList<Block>();
+		for (Piece pc : pieces) {
+			for (PieceMap map : pc.positions) {
+				blks.add(new Block(btIndex,map.pieceMap));
+			}
+			btIndex++;
+		}
+		return(blks.toArray(new Block[blks.size()]));
+	}
+	
+	
+
+	
+	
 	public static class Block {
-		int blockIndex,blockTypeIndex;
-		long blockPattern,requiresPattern,availShifts;
-		long myBlockMask,similarBlockMask;
-		public Block(int index,int btIndex,long simlMask,Boolean[][] piece) {
-			blockIndex=index;
-			blockTypeIndex=btIndex;
-			myBlockMask=(1L<<index);
-			similarBlockMask=simlMask;
-			blockPattern=0;
-			long bit=1;
-			for (int y=0;y<8;y++) {
-				for (int x=0;x<8;x++,bit<<=(blockPattern==0?0:1)) {
-					if ((piece.length>y)&&(piece[y].length>x)&&piece[y][x]) {
-						blockPattern|=bit;
-					}
-				}
-			}
-			requiresPattern=0;
+		int blockTypeIndex;
+		long blockPattern,availShifts;
+		
+		public Block(int blockType,Boolean[][] piece) {
+			blockTypeIndex=blockType;
+			blockPattern=patternFromArray(piece);
 			availShifts=0;
-			bit=1;
-			long avaiBdMask=0xffffffffffffffffL;
-			for (int shift=0;shift<8*8;avaiBdMask&=~(1L<<(63-shift)),shift++,bit<<=1) {
-				if (((avaiBdMask&blockPattern)==blockPattern)&&
-						!(
-								(((blockPattern<<shift)&0x0101010101010101L)!=0)&&
-								(((blockPattern<<shift)&0x8080808080808080L)!=0))
-								) {
-					availShifts|=bit;
+			for (int shift=0;shift<64;shift++) {
+				if (canShiftWithoutWrappingOrClipping(blockPattern,shift)) {
+					availShifts|=(1L<<shift);
 				}
 			}
-			
-			String simls="";
-			for (int i=0;i<64;i++) if ((this.similarBlockMask&(1L<<i))!=0) simls+=""+i+",";
-			
+			//System.out.println(toString());
+		}
+		
+		
+		public String toString() {
 			String s="",shfts="";
 			for (int y=0;y<8;y++) {
 				for (int x=0;x<8;x++) {
@@ -46,62 +75,66 @@ public class LongPlay {
 				s+="\n";
 				shfts+="\n";
 			}
-			System.out.println("Block "+blockIndex+"  (siml : "+simls+")\n"+s+"Shifts:\n"+shfts+"\n");
+			return("Block "+blockTypeIndex+"\n"+s+"Shifts:\n"+shfts);
 		}
 	}
-	
 	
 	
 	
 	public static class Board {
-		int boardIndex,parentIndex;
+		Board parent;
 		long used,usedBlockMask;
-	}
-	
-	
-	
-	public static class SelfContainedBoard {
-		int boardIndex;
-		SelfContainedBoard parent;
-		long used,usedBlockMask;
-		int blockTypeIndex;
+		int addedBlockTypeIndex;
+		int blockTypeAtTL,blockTypeAtTR;
 		
-		public SelfContainedBoard(int copyIndex,Board[] boards) {
-			Board copy=boards[copyIndex];
-			used=copy.used;
-			usedBlockMask=copy.usedBlockMask;
-			blockTypeIndex=-1;
-			parent=(copy.boardIndex<0?
-					null:
-						new SelfContainedBoard(copy.boardIndex,boards)
-			);
-		}
-
-		public SelfContainedBoard() {
+		public Board() {
 			parent=null;
 			used=usedBlockMask=0;
-			blockTypeIndex=-1;
-		}
-		private SelfContainedBoard(SelfContainedBoard parent,long used,long usedBlockMask,int btIndex) {
-			this.parent=parent;
-			this.blockTypeIndex=btIndex;
-			this.used=used;
-			this.usedBlockMask=usedBlockMask;
+			addedBlockTypeIndex=-1;
 		}
 		
-		public SelfContainedBoard placeBlock(Block blk) {
-			if ((usedBlockMask&blk.similarBlockMask)!=0) return(null);
-			int shift=min1(~used);long bit=(1L<<shift);
-			return (((blk.availShifts&bit)==0)||
-					((used&(blk.blockPattern<<shift))!=0)||
-					((used&(blk.requiresPattern<<shift))!=(blk.requiresPattern<<shift))?
-							null:
-								new SelfContainedBoard(this,used|(blk.blockPattern<<shift),usedBlockMask|blk.myBlockMask,blk.blockTypeIndex));
+		
+		private Board(Board parent,Block block,int shift) {
+			this.parent=parent;
+			long blkp=(block.blockPattern<<shift);
+			blockTypeAtTL=((blkp&0x01)!=0?block.blockTypeIndex:parent.blockTypeAtTL);
+			blockTypeAtTR=((blkp&0x80)!=0?block.blockTypeIndex:parent.blockTypeAtTR);
+			used=blkp|parent.used;
+			usedBlockMask=(1L<<block.blockTypeIndex)|parent.usedBlockMask;
+			addedBlockTypeIndex=block.blockTypeIndex;
+			
+			//System.out.println(toString());
+		}
+		
+		
+		public Board placeBlock(Block block) {
+			if ((usedBlockMask&(1L<<block.blockTypeIndex))!=0) {
+				return(null); // i.e. block has been used by this board or parents
+			}
+			int shift=min0(used); // we want to fill the next available 0 in the board
+														// note that all blocks have been set with bit0=1
+			long blkp=(block.blockPattern<<shift);
+			if (((block.availShifts&(1L<<shift))==0)|| // the block would clip or wrap
+					((used&blkp)!=0)) {  // the block doesn't fit
+				return(null);   // no go
+			}
+			// should be (TL>TR)&&(BR>TR)&&(BL>TR)&&(TL<BR)
+			if ((blkp&0x80)!=0) { // detecting rotation step one
+				if (block.blockTypeIndex>blockTypeAtTL) return(null);
+			}
+			else if ((blkp&0x8000000000000000L)!=0) { // detecting rotation step two
+				if ((block.blockTypeIndex<blockTypeAtTL)||
+						(block.blockTypeIndex<blockTypeAtTR)) return(null);
+			}
+			else if ((blkp&0x0100000000000000L)!=0) { // detecting rotation step three
+				if (block.blockTypeIndex<blockTypeAtTR) return(null);
+			}
+			return(new Board(this,block,shift));  // new board
 		}
 		
 		public char[][] toArray() {
 			long parUsed=(parent==null?0:parent.used);
-			char c=(char)(blockTypeIndex+'a');
+			char c=(char)(addedBlockTypeIndex+'A');
 			char[][] ret;
 			if (parent==null) {
 				ret=new char[8][];
@@ -120,7 +153,6 @@ public class LongPlay {
 		public String toString() {
 			char[][] map=toArray();
 			String ret="";
-			if ((map[0][0]>map[7][0])||(map[0][0]>map[7][7])||(map[0][0]>map[0][7])||(map[0][7]>map[7][0])) return("duplicate");
 			for (char[] ca : map) {
 				for (char c : ca) ret+=""+c;
 				ret+="\n";
@@ -129,25 +161,11 @@ public class LongPlay {
 		}
 	}
 	
-	Block[] blocks=stGrabBlocks();
-	
-	public static Block[] stGrabBlocks() {
-		List<Piece> pieces=PieceFactory.puzzlePieces1();
-		int index=0;
-		int btIndex=0;
-		List<Block> blks=new ArrayList<Block>();
-		for (Piece pc : pieces) {
-			long simlMask=((1L<<(index+pc.positions.length))-1)-((1L<<(index))-1);
-			for (PieceMap map : pc.positions) {
-				blks.add(new Block(index++,btIndex,simlMask,map.pieceMap));
-			}
-			btIndex++;
-		}
-		return(blks.toArray(new Block[blks.size()]));
-	}
 	
 	
-	List<SelfContainedBoard> winningBoardList=new ArrayList<SelfContainedBoard>();
+	
+	
+	List<Board> winningBoardList=new ArrayList<Board>();
 	
 	
 	
@@ -166,15 +184,16 @@ public class LongPlay {
 		public void run() {
 			progress=0;
 			long start=System.currentTimeMillis();
-			doDFS(new SelfContainedBoard(),1.0/lp.blocks.length);
+			doDFS(new Board(),1.0/lp.blocks.length);
 			long end=System.currentTimeMillis();
-			System.out.println("\n\nFound "+lp.winningBoardList.size()+" winning boards in "+(0.001*(end-start))+" seconds\n");
+
+			System.out.println("\n\n"+resultString(11,lp.winningBoardList)+"Found "+lp.winningBoardList.size()+" winning boards in "+(0.001*(end-start))+" seconds\n");
 		}
-		public void doDFS(SelfContainedBoard bd,double addProgress) {
+		public void doDFS(Board bd,double addProgress) {
 			if (addProgress<0.1) doDFSWProgress(bd,addProgress);
 			else {
 				for (Block blk : lp.blocks) {
-					SelfContainedBoard bd2=bd.placeBlock(blk);
+					Board bd2=bd.placeBlock(blk);
 					if (bd2!=null) {
 						doDFS(bd2,addProgress/lp.blocks.length);
 					}
@@ -182,9 +201,9 @@ public class LongPlay {
 				}
 			}
 		}
-		public void doDFSWProgress(SelfContainedBoard bd,double addProgress) {
+		public void doDFSWProgress(Board bd,double addProgress) {
 			for (Block blk : lp.blocks) {
-				SelfContainedBoard bd2=bd.placeBlock(blk);
+				Board bd2=bd.placeBlock(blk);
 				if (bd2!=null) {
 					doDFS(bd2);
 				}
@@ -192,16 +211,13 @@ public class LongPlay {
 				System.out.println("Progress: "+(progress*100)+"%");
 			}
 		}
-		public void doDFS(SelfContainedBoard bd) {
+		public void doDFS(Board bd) {
 			for (Block blk : lp.blocks) {
-				SelfContainedBoard bd2=bd.placeBlock(blk);
+				Board bd2=bd.placeBlock(blk);
 				if (bd2!=null) {
 					if (bd2.used==0xffffffffffffffffL) {
-						String s=bd2.toString();
-						if (!s.equals("duplicate")) {
-							lp.winningBoardList.add(bd2);
-							System.out.println("Found:\n"+s);
-						}
+						lp.winningBoardList.add(bd2);
+						System.out.println("Found winner #"+lp.winningBoardList.size());
 					}
 					else doDFS(bd2);
 				}
@@ -211,12 +227,49 @@ public class LongPlay {
 	
 	
 	
+	public static String resultString(int width,List<Board> boards) {
+		String ret="";
+		String[] lines=new String[8];
+		for (int i=0;i<8;i++) lines[i]="";
+		for (int bdi=0;bdi<boards.size();) {
+			for (int j=0;(j<width)&&(bdi<boards.size());j++,bdi++) {
+				String[] bdLines=boards.get(bdi).toString().split("\n");
+				for (int i=0;i<8;i++) lines[i]+=(lines[i].length()==0?"":"   ")+bdLines[i];
+			}
+			for (int i=0;i<8;i++) {
+				ret+=lines[i]+"\n";
+				lines[i]="";
+			}
+			ret+="\n";
+		}
+		return(ret);
+	}
+	
 	public static short min1_16bit[]=new short[0x10000];
 	static {
 		for (int b=1,bi=0;bi<16;bi++,b<<=1) {
 			for (int msk=b,b2=b<<1;msk<0x10000;msk+=b2) min1_16bit[msk]=(short)bi;
 		}
 		min1_16bit[0]=0x1000;
+		
+		if (doTests) {
+			Random random=new Random();
+			for (int i=0;i<1000;i++) {
+				long l=random.nextLong();
+				int bi;
+				for (bi=0;bi<64;bi++) if ((l&(1L<<bi))!=0) break;
+				if (bi<64?(min1(l)!=bi):(min1(l)<100)) {
+					System.out.println("Min1 wrong ("+l+" should be "+bi+" not "+min1(l));
+				}
+				for (bi=0;bi<64;bi++) if ((l&(1L<<bi))==0) break;
+				if (bi<64?(min0(l)!=bi):(min0(l)<100)) {
+					System.out.println("Min0 wrong ("+l+" should be "+bi+" not "+min1(l));
+				}
+			}
+		}
+	}
+	public static int min0(long msk) {
+		return(min1(~msk));
 	}
 	public static int min1(long msk) {
 		return(Math.min(
